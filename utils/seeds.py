a5a6d523f4508cedeaac3a298e6f6bd09fb2f550
AndPewka <sayan-andrey@mail.ru>
2023-03-31 17:34:06 +0700

    update generate django key

--- utils/seeds.py
+++ utils/seeds.py
@@ -4,10 +4,13 @@ import itertools
 
 from django import setup
 from django.apps import apps
+from django.core.management.utils import get_random_secret_key
+from dotenv import set_key, load_dotenv
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 setup()
+load_dotenv()
 
 def generate_default_currency():
     Service = apps.get_model("telegram_bot", "Service", require_ready=True)
@@ -22,5 +25,13 @@ def generate_default_currency():
         if created:
             print(f"create pair {currency} on {service}")
 
+def generate_django_secret():
+    if os.getenv('DJANGO_SECRET_KEY'):
+        return False
+
+    print(f"create DJANGO_SECRET_KEY")
+    set_key('.env', 'DJANGO_SECRET_KEY', get_random_secret_key())
+
 if __name__ == '__main__':
     generate_default_currency()
+    generate_django_secret()
